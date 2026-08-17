133d570e0f210e1152fc82f02b066e52fdbb7a27
Ebinezar Dhanaraj <ebiiscoding@gmail.com>
2026-08-18 02:00:57 +0530

    feat: add full 365-menu guided cookbook

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lg-mj3286brus-v6';
+const CACHE='lg-mj3286brus-v8';
 const ASSETS=['./','./index.html','./manifest.json','./icon-180.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
