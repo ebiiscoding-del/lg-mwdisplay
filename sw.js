589e5ecbd4808433524e8ca5b49a3121a017e42e
Ebinezar Dhanaraj <ebiiscoding@gmail.com>
2026-08-18 01:49:04 +0530

    feat: add guided cookbook and cook timer

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
-const CACHE='lg-mj3286brus-v1';
+const CACHE='lg-mj3286brus-v6';
 const ASSETS=['./','./index.html','./manifest.json','./icon-180.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
